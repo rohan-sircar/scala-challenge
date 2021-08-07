d2ba5828eebe5e5afabe6cd8c268dcffad5756f4
Rohan Sircar <rohansircar@tuta.io>
2021-08-07 15:43:00 +0530

    cleaup main

--- src/main/scala/wow/doge/http4sdemo/Main.scala
+++ src/main/scala/wow/doge/http4sdemo/Main.scala
@@ -1,16 +1,11 @@
 package wow.doge.app
 
-import scala.concurrent.duration.MILLISECONDS
-
 import cats.effect.ExitCode
 import cats.effect.Resource
-import cats.syntax.all._
 import monix.bio.BIOApp
-import monix.bio.IO
 import monix.bio.Task
 import monix.bio.UIO
 import monix.execution.Scheduler
-import io.odin.consoleLogger
 
 object Main extends BIOApp {
   val schedulers = Schedulers.default
@@ -18,16 +13,7 @@ object Main extends BIOApp {
   override protected def scheduler: Scheduler = schedulers.async.value
 
   val program = for {
-    startTime <- Resource.eval(IO.clock.realTime(MILLISECONDS))
-    _ <- Resource.eval(Task(println("""
-    |        .__     __    __            _____                      .___                     
-    |        |  |___/  |__/  |_______   /  |  |  ______           __| _/____   _____   ____  
-    |        |  |  \   __\   __\____ \ /   |  |_/  ___/  ______  / __ |/ __ \ /     \ /  _ \ 
-    |        |   Y  \  |  |  | |  |_> >    ^   /\___ \  /_____/ / /_/ \  ___//  Y Y  (  <_> )
-    |        |___||_|__|  |__| |   __/\____   |/______|         \_____|\____/\__|_|_/ \____/ 
-    |                          |__|        |__|                                        
-     """.stripMargin)))
-    // logger = consoleLogger[Task]()
+    _ <- Resource.eval(Task.unit)
     appRoutes = new AppRoutes
     _ <- new Server(schedulers, appRoutes).resource
   } yield ()
